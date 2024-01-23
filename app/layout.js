d05b1a36c0b19ad4d6b7fb6e9568e8dafeca58d4
rotanakkosal03 <rotanakkosal03@gmail.com>
2024-01-23 11:55:35 +0700

    Add Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import Provider from "@/context/Provider";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
